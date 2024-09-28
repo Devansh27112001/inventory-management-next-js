c7f13df4f64d6301746fe65f4c6735c45980a2f4
Devansh27112001 <dkansara2711@gmail.com>
2024-09-28 13:22:10 -0400

    CLIENT:Setting up our redux store and working with the Sidebar functionality

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,16 +1,30 @@
 "use client";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
 import { Menu } from "lucide-react";
 
 const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const sidebarStyle = `fixed flex flex-col ${
+    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+  } bg-white transition-all duration-300 ease-in-out h-full shadow-md z-40 overflow-hidden`;
   return (
-    <div>
+    <div className={sidebarStyle}>
       {/* TOP LOGO */}
       <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
         <div>Logo</div>
         <h1 className="font-extrabold text-2xl">EDSTOCK</h1>
         <button
           className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => {}}
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
         </button>
