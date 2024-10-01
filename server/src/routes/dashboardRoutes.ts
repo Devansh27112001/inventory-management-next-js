759304e10d9928c9889cc7eaa69c6715128a7487
Devansh27112001 <dkansara2711@gmail.com>
2024-10-01 11:44:06 -0400

    CLIENT: working on the grid layout for dashboard page

--- server/src/routes/dashboardRoutes.ts
+++ server/src/routes/dashboardRoutes.ts
@@ -2,5 +2,5 @@ import { Router } from "express";
 import { getDashboard } from "../controllers/dashboardController";
 
 const router = Router();
-router.get("/metrics", getDashboard); // http://localhost:8000/dashboard/metrics
+router.get("/", getDashboard); // http://localhost:8000/dashboard/
 export default router;
