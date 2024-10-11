1501988c9c13ecde6cd42362e56dcbf67da3ff6e
Devansh27112001 <dkansara2711@gmail.com>
2024-10-10 21:25:52 -0400

    CLIENT: expenses page completed: Was a little intense

--- client/src/app/expenses/page.tsx
+++ client/src/app/expenses/page.tsx
@@ -1,13 +1,32 @@
 "use client";
 
-import { useGetExpensesByCategoryQuery } from "@/state/api";
+import {
+  ExpenseByCategorySummary,
+  useGetExpensesByCategoryQuery,
+} from "@/state/api";
 import { useMemo, useState } from "react";
 import Header from "@/app/(components)/Header";
-import { Pie, PieChart, ResponsiveContainer } from "recharts";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
 
+type AggregatedDataItem = {
+  name: string;
+  color?: string;
+  amount: number;
+};
+
+type AggregatedData = {
+  [category: string]: AggregatedDataItem;
+};
 const Expenses = () => {
   const [activeTab, setActiveTab] = useState(0);
-  const [selectedCaregory, setSelectedCategory] = useState("All");
+  const [selectedCategory, setSelectedCategory] = useState("All");
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
 
@@ -19,10 +38,40 @@ const Expenses = () => {
 
   const expenses = useMemo(() => expensesData ?? [], [expensesData]);
 
+  const parseByDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toISOString().split("T")[0];
+  };
+  const aggregatedData: AggregatedDataItem[] = useMemo(() => {
+    const filtered: AggregatedData = expenses
+      .filter((data: ExpenseByCategorySummary) => {
+        const matchedCategory =
+          selectedCategory === "All" || data.category === selectedCategory;
+        const dataDate = parseByDate(data.date);
+        const matchesDate =
+          !startDate ||
+          !endDate ||
+          (dataDate >= startDate && dataDate <= endDate);
+        return matchedCategory && matchesDate;
+      })
+      .reduce((acc: AggregatedData, data: ExpenseByCategorySummary) => {
+        const amount = parseInt(data.amount);
+        if (!acc[data.category]) {
+          acc[data.category] = {
+            name: data.category,
+            amount: 0,
+            color: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+          };
+          acc[data.category].amount += amount;
+        }
+        return acc;
+      }, {});
+    return Object.values(filtered);
+  }, [expenses, selectedCategory, startDate, endDate]);
   const classNames = {
     label: "block text-sm font-medium text-gray-700",
     selectInput:
-      "mt-1 block w-full pl-3 py-2 pr-10 text-base border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md transition duration-300 ease-in-out",
+      "mt-1 block w-full pl-3 py-2 pr-2 text-base border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md transition duration-300 ease-in-out",
   };
 
   if (isLoading) return <div className="py-4">Loading...</div>;
@@ -103,7 +152,31 @@ const Expenses = () => {
         <div className="flex-grow bg-white shadow rounded-lg p-4 md:p-6">
           <ResponsiveContainer width="100%" height={400}>
             <PieChart>
-              <Pie />
+              <Pie
+                data={aggregatedData}
+                dataKey="amount"
+                cx="50%"
+                cy="50%"
+                label
+                outerRadius={150}
+                fill="#8884d8"
+                onMouseEnter={(_, index) => {
+                  setActiveTab(index);
+                }}
+              >
+                {aggregatedData.map(
+                  (entry: AggregatedDataItem, index: number) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={
+                        index === activeTab ? "rgb(29, 78, 216)" : entry.color
+                      }
+                    />
+                  )
+                )}
+              </Pie>
+              <Tooltip />
+              <Legend />
             </PieChart>
           </ResponsiveContainer>
         </div>
