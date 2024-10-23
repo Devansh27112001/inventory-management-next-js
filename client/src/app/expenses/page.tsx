0648b90b164cfad861f695524074345ffef0835c
Devansh27112001 <dkansara2711@gmail.com>
2024-10-23 16:26:18 -0400

    Final commit

--- client/src/app/expenses/page.tsx
+++ client/src/app/expenses/page.tsx
@@ -72,7 +72,7 @@ const Expenses = () => {
     label: "block text-sm font-medium text-gray-700",
     selectInput:
       "mt-1 block w-full pl-3 py-2 pr-2 text-base border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md transition duration-300 ease-in-out",
-  };
+  }; 
 
   if (isLoading) return <div className="py-4">Loading...</div>;
 
